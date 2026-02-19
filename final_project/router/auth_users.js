6c640b9d038e684bdc827d76b82bc1c46e2b8e27
CmdCoffee <hello@antoinekuijl.com>
2026-02-19 13:39:43 -0500

    Save task 2.8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -62,8 +62,20 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const reviewInput = req.query.review
+    const isbn = req.params.isbn
+    const username = req.session.authorization.username
+
+    if (username && isbn && reviewInput) {
+        if(books[isbn]) {
+            books[isbn].reviews[username] = reviewInput;
+            return res.status(200).json({message: "Review posted"});
+        } else {
+            return res.status(404).json({message: "No ISBN match"})
+        };
+        } else {
+    return res.status(404).json({message: "Invalid Input"});
+    }
 });
 
 module.exports.authenticated = regd_users;
